9d9eb61dda183a2cecc1875a756c3c20f41bfc5d
seoyoungjin23 <ascd050@gmail.com>
2025-03-15 01:12:26 +0900

    test: 파일 업로드/다운로드 테스트
    AWS S3 스토리지를 활용한 파일 업로드/다운로드를 테스트한다.

---
+++ src/main/java/com/ky/docstory/repository/FileRepository.java
@@ -0,0 +1,10 @@
+package com.ky.docstory.repository;
+
+import com.ky.docstory.entity.File;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileRepository extends JpaRepository<File, Long> {
+
+}
