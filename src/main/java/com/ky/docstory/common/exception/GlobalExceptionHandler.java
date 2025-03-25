17088ec0231df6c4cff59e194ff45b05a72c705d
kanguk <lm0828@naver.com>
2025-03-25 17:31:02 +0900

    feat: 유효성 검증 추가
    레포지토리 이름에 대해 유효성 검증을 진행하도록 한다. (컨트롤러 단에서 진행)

--- src/main/java/com/ky/docstory/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ky/docstory/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.ky.docstory.common.exception;
 import com.ky.docstory.common.code.DocStoryResponseCode;
 import com.ky.docstory.common.dto.DocStoryResponseBody;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,6 +17,17 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(errorCode.getHttpStatus())
                 .body(DocStoryResponseBody.error(errorCode.getCode(), errorCode.getMessage()));
+    }
+
+    // 컨트롤러 유효성 검증
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<DocStoryResponseBody<Void>> handleValidationException(MethodArgumentNotValidException e) {
+        return ResponseEntity
+                .status(DocStoryResponseCode.VALIDATION_ERROR.getHttpStatus())
+                .body(DocStoryResponseBody.error(
+                        DocStoryResponseCode.VALIDATION_ERROR.getCode(),
+                        e.getBindingResult().getAllErrors().get(0).getDefaultMessage() // 첫 번째 메시지만 노출
+                ));
     }
 
     // 그 외 일반 예외 처리
