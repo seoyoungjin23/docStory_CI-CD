c22be68479449d7b9350aa488396a0693fdd5ae3
kanguk <lm0828@naver.com>
2025-03-30 13:48:17 +0900

    feat: N+1 문제 해결
    N+1 문제를 해결하여 쿼리를 최적화한다.

--- src/main/java/com/ky/docstory/repository/TeamRepository.java
+++ src/main/java/com/ky/docstory/repository/TeamRepository.java
@@ -4,6 +4,8 @@ import com.ky.docstory.entity.Repository;
 import com.ky.docstory.entity.Team;
 import com.ky.docstory.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +13,9 @@ import java.util.UUID;
 
 public interface TeamRepository extends JpaRepository<Team, UUID> {
     boolean existsByRepositoryAndUser(Repository repository, User user);
+
+    @Query("SELECT t FROM Team t JOIN FETCH t.user WHERE t.repository = :repository")
+    List<Team> findAllByRepositoryWithUser(@Param("repository") Repository repository);
 
     List<Team> findAllByRepository(Repository repository);
 
