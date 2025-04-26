c52bdf90ee6e2894790b85e63beb7171628069ef
seoyoungjin23 <ascd050@gmail.com>
2025-04-26 18:24:06 +0900

    fix: N+1 문제로 인한 코드 수정
    즐겨찾기 목록 조회에서 N+1 문제가 발생할 수 있는 요소가 없도록 쿼리를 수정한다.

--- src/main/java/com/ky/docstory/repository/FavoriteRepository.java
+++ src/main/java/com/ky/docstory/repository/FavoriteRepository.java
@@ -7,11 +7,14 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, UUID> {
     Optional<Favorite> findByUserAndRepository(User user, Repository repository);
 
     boolean existsByUserAndRepository(User user, Repository repository);
 
-    List<Favorite> findAllByUser(User user);
+    @Query("SELECT f FROM Favorite f JOIN FETCH f.repository r WHERE f.user = :user")
+    List<Favorite> findAllByUser(@Param("user") User user);
 }
