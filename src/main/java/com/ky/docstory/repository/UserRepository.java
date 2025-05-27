90640ae66abd9d53f58beccb215c02e33b1f4a2e
seoyoungjin23 <ascd050@gmail.com>
2025-05-27 15:38:22 +0900

    feat: 내 정보 조회, 팀원 초대 API에 이메일 추가

--- src/main/java/com/ky/docstory/repository/UserRepository.java
+++ src/main/java/com/ky/docstory/repository/UserRepository.java
@@ -8,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByProviderId(String providerId);
+
+    Optional<User> findByEmail(String email);
 }
